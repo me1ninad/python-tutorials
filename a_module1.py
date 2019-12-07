c6e4536abdab0bb09f5812aeb193f4cf44dc8338
me1ninad <baraskar.ninad@gmail.com>
2019-12-07 19:21:51 +0530

    ai analytics demo

---
+++ a_module1.py
@@ -0,0 +1,12 @@
+a_num=10
+a_str="python"
+a_list=[10,20,30,40]
+a_float=10.45
+def square(x):
+    return x*x
+
+def fibo(x):
+    a,b=0,1
+    while b<n:
+        print(b,end=" ")
+        a,b=b,a+b
